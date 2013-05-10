038bdd075801634a90ad0595b007f77ef0a91790
Emanuel Lauria <emanuel.lauria@gmail.com>
2013-05-10 18:05:19 +0200

    slider implemented

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ $(function() {
     });
 
     $('a#info').click(function() {
-        console.log('clicked');
         if ($('a#info').hasClass('visible')) {
             $('.info').hide(500);
             return $('a#info').html('more info?').removeClass('visible');
@@ -18,7 +17,81 @@ $(function() {
         $('.info').show(1000);
         $('a#info').html('less info').addClass('visible');
     });
+
+    $('#slideshow_right').click(function() {
+        stopAnimation();
+        nextSlide();
+    });
+
+    $('#slideshow_left').click(function() {
+        stopAnimation();
+        prevSlide();
+    });
+
+    $('#indicators li span').click(function(e) {
+        var $e = $(e.currentTarget);
+        var id = $e.attr('id');
+        stopAnimation();
+        jump(id);
+    });
+
+    startSlider();
+
 });
+
+var slider  = {
+    "pic_size": 816,
+    "speed": 700,
+    "interval": 10000,
+    "pid": null,
+    "wait": null
+}
+
+function nextSlide() {
+    var left = $('#slides').css('margin-left').split('px')[0];
+    var i = parseInt(Math.abs(left) / slider.pic_size) + 1;
+    left = i * slider.pic_size * -1;
+    $('#slides').stop(true, true);
+    $('#slides').animate({ 'margin-left': left}, slider.speed, function() {
+        setPointer(i);
+        if (i == 7) { return resetSlider(); }
+    });
+}
+
+function prevSlide() {
+    var left = $('#slides').css('margin-left').split('px')[0];
+    var i = parseInt(Math.abs(left) / slider.pic_size) - 1;
+    left = i * slider.pic_size * -1;
+    $('#slides').stop(true, true);
+    if (i == -1) { return setLastSlide(); };
+    $('#slides').animate({ 'margin-left': left}, slider.speed, function() {
+        setPointer(i);
+        if (left >= 0) { return setLastSlide(); }
+    });
+}
+
+function jump(id) {
+    var left = id * slider.pic_size * -1;
+    $('#slides').animate({ 'margin-left': left}, slider.speed, function() {
+        setPointer(id);
+    });
+
+}
+
+function setPointer(i) {
+    $('#indicators .active_ind').removeClass('active_ind');
+    $('#indicators #'+i).addClass('active_ind');
+}
+
+function resetSlider() {
+    $('#slides').css('margin-left', slider.pic_size * -1);
+    setPointer(1);
+}
+
+function setLastSlide() {
+    $('#slides').css('margin-left', 6 * slider.pic_size * -1);
+    setPointer(6);
+}
 
 function showExample() {
     $('#example').addClass('visible');
@@ -34,4 +107,16 @@ function hideExample() {
     $('#demo_banner').removeClass('open');
     $('#example').slideUp(1000);
     $('html, body').animate({ scrollTop: 0 }, 1000);
+}
+
+function startSlider() {
+    slider.pid = setInterval(function() {
+        nextSlide();
+    }, slider.interval);
+}
+
+function stopAnimation() {
+    clearInterval(slider.pid);
+    clearTimeout(slider.wait);
+    slider.wait = setTimeout(startSlider, 5000);
 }
